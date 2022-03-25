bb8b516316e7eddfef104d6466ed66c10cc329db
hmcvay <hayleymcvay8@gmail.com>
2022-03-25 13:42:42 -0700

    Add complete treats controller, and add ApplicationUser to treat model.

--- TreatsNSweets/Models/Treat.cs
+++ TreatsNSweets/Models/Treat.cs
@@ -13,6 +13,8 @@ namespace TreatsNSweets.Models
     public int TreatId { get; set; }
     public string Name { get; set; }
 
+    public virtual ApplicationUser User { get; set; }
+
     public virtual ICollection<FlavorTreat> JoinEntities { get; set; }
 
   }
